abae0d87a441e289afc78cee2a422d3599f1733f
David Richardson <david@tankski.co.uk>
2019-02-05 12:58:50 +0000

    Finish DFS impl

--- java/org/contikios/cooja/util/NodeGraph.java
+++ java/org/contikios/cooja/util/NodeGraph.java
@@ -4,11 +4,12 @@ import org.contikios.cooja.Mote;
 import org.contikios.cooja.Simulation;
 import org.contikios.cooja.radiomediums.UDGM;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class NodeGraph {
     private Map<Mote, Map<Mote, Integer>> matrix;
+    private Map<Mote, Boolean> activeMotes;
 
     public NodeGraph(Simulation simulation) {
         refreshMatrix(simulation);
@@ -23,6 +24,10 @@ public class NodeGraph {
 
         if (this.matrix == null || getMatrixSize() != motes.length) {
             this.matrix = new HashMap<>(motes.length);
+        }
+
+        if (this.activeMotes == null) {
+            this.activeMotes = new HashMap<>(motes.length);
         }
 
         for (Mote src : motes) {
@@ -48,7 +53,80 @@ public class NodeGraph {
             }
 
             matrix.put(src, row);
+            activeMotes.put(src, true);
         }
+    }
+
+    public void toggleMote(Mote mote) {
+        // Iterate over the map and set t
+        for (Mote m : this.matrix.keySet()) {
+            Map<Mote, Integer> row = this.matrix.get(m);
+
+            if (m.getID() == mote.getID()) {
+                for (Mote k : row.keySet()) {
+                    row.put(k, -1*row.get(k));
+                }
+            } else {
+                for (Mote k : row.keySet()) {
+                    if (k.getID() == mote.getID()) {
+                        row.put(k, -1*row.get(k));
+                    }
+                }
+            }
+        }
+
+        boolean status = this.activeMotes.get(mote);
+        this.activeMotes.put(mote, !status);
+    }
+
+    private Set<Mote> getActiveMotes() {
+        return this.activeMotes.entrySet().stream()
+                .filter(Map.Entry::getValue)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+    }
+
+    public boolean isConnected() {
+        Set<Mote> visited = new HashSet<>(matrix.size());
+
+        Deque<Mote> path = new ArrayDeque<>();
+        path.push(matrix.keySet().iterator().next());
+
+        while (!path.isEmpty()) {
+            Mote current = path.peek();
+
+            visited.add(current);
+            // Valid neighbours are ones that have not been visited yet
+            Set<Mote> neighbours = getNeighbours(current);
+            neighbours.removeAll(visited);
+
+            if (neighbours.isEmpty()) {
+                path.pop();
+            } else {
+                path.push(neighbours.iterator().next());
+            }
+        }
+
+        return visited.size() == getActiveMotes().size();
+    }
+
+    private Set<Mote> getNeighbours(Mote mote) {
+        HashSet<Mote> neighbours = new HashSet<>();
+
+        Map<Mote, Integer> row = this.matrix.get(mote);
+
+        for (Mote m : row.keySet()) {
+            if (row.get(m) > 0) {
+                neighbours.add(m);
+            }
+        }
+
+        return neighbours;
+    }
+
+    @Override
+    public String toString() {
+        return matrix.toString();
     }
 
     private int getMatrixSize() {
@@ -58,12 +136,4 @@ public class NodeGraph {
             return -1;
         }
     }
-
-    @Override
-    public String toString() {
-        return matrix.toString();
-    }
-
-    // TODO: Add APIs for adding/removing nodes to the graph, and updating if a
-    // mote has been made offline or not.
 }
